3db014f0c011a69d4ea18cacf803b35241344b9a
Pandi <rlqmalej@gmail.com>
2021-04-16 16:51:55 +0900

    Title UI (No Image) 초안 제작

---
+++ Isolate-Island/Assets/Script/SceneChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum SceneNumber
+{
+    Title, InGame
+}
+
+public class SceneChange : MonoBehaviour
+{
+    public void OnpressedPlayButton(SceneNumber scene)
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+
+    public void _Title()
+    {
+        
+        SceneManager.LoadScene((int) SceneNumber.Title);    
+    }
+    public void _InGame()
+    {
+        SceneManager.LoadScene((int) SceneNumber.InGame);    
+    }
+
+}
