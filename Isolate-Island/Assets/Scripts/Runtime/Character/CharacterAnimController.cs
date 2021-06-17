ed462393350b358cff10214bdcd8a81813fb9871
박현재 박 <md101106@gmail.com>
2021-06-17 23:51:20 +0900

    IK 패키지 추가 & 플레이어 이동 애니메이션 제작

--- Isolate-Island/Assets/Scripts/Runtime/Character/CharacterAnimController.cs
+++ Isolate-Island/Assets/Scripts/Runtime/Character/CharacterAnimController.cs
@@ -4,6 +4,8 @@ namespace IsolateIsland.Runtime.Character
 {
     public class CharacterAnimController : CharacterController
     {
+        [SerializeField] private bool isFliping = false;
+
         [SerializeField] private Animator animator;
         public Animator Animator
         {
@@ -31,7 +33,7 @@ namespace IsolateIsland.Runtime.Character
                 return;
             }
 
-            var flipX = (xAxis > 0) ? 0f : -180f;
+            var flipX = ((isFliping) ? (xAxis > 0) : (xAxis < 0)) ? 0f : -180f;
             transform.rotation = Quaternion.Euler(0, flipX, 0);
 
             Animator.SetBool("isRun", true);
