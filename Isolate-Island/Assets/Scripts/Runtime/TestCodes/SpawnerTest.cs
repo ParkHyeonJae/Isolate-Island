a21ab6bd02c135028fbcc52613b037452f17eed2
LalDi <jungjh0513@naver.com>
2021-06-24 15:58:04 +0900

    Add SpawnerTest.cs

---
+++ Isolate-Island/Assets/Scripts/Runtime/TestCodes/SpawnerTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IsolateIsland.Runtime.TestCodes
+{
+    [ExecuteInEditMode]
+    public class SpawnerTest : MonoBehaviour
+    {
+        public enum Selector
+        {
+            Enemy,
+            Item,
+        }
+
+        public Selector nowSpawn;
+    }
+}
