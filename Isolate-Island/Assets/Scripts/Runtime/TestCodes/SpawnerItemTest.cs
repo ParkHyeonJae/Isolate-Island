36cff176ee210d432e05d4342ce753cc953691d0
LalDi <jungjh0513@naver.com>
2021-06-25 16:05:40 +0900

    Add SpawnerItemTest.cs

---
+++ Isolate-Island/Assets/Scripts/Runtime/TestCodes/SpawnerItemTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IsolateIsland.Runtime.Inventory;
+using IsolateIsland.Runtime.Combination;
+
+namespace IsolateIsland.Runtime.TestCodes
+{
+    [ExecuteInEditMode]
+    public class SpawnerItemTest : MonoBehaviour
+    {
+        public Transform spawnRangeObject;
+        public float spawnRange
+        {
+            get
+            {
+                return Vector3.Distance(spawnRangeObject.position, gameObject.transform.position);
+            }
+        }
+
+        public List<CombinationNode> spawnObject = new List<CombinationNode>();
+
+        private void Start()
+        {
+            ItemBuilder itemBuilder = new ItemBuilder();
+            GameObject itemBase = itemBuilder.
+                SetCombinationNode(spawnObject[0]).
+                Build().
+                gameObject;
+
+            itemBase.transform.position = new Vector3(5, 0, 0);
+        }
+    }
+}
