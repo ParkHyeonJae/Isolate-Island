344e90830b34cbb5dd6a388235640dea13ca21af
박현재 박 <md101106@gmail.com>
2021-07-07 04:27:12 +0900

    Add Light&Night Scene

---
+++ Isolate-Island/Assets/Scripts/Runtime/Light/DayNightConfigurator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+namespace IsolateIsland.Runtime.Light
+{
+    public class DayNightConfigurator : MonoBehaviour
+    {
+        [SerializeField] Light2D globalLight;
+        // Start is called before the first frame update
+        void Start()
+        {
+            //Managers.Managers.Instance.GameManager.isDay;
+        }
+    }
+}
