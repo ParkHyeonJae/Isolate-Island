13969f83268b97e121806b9ba682b2920ca182af
LalDi <jungjh0513@naver.com>
2021-07-07 15:11:24 +0900

    돼지 애니메이션 추가

--- Isolate-Island/Assets/Scripts/Runtime/Character/Entitys/EnemyEntity.cs
+++ Isolate-Island/Assets/Scripts/Runtime/Character/Entitys/EnemyEntity.cs
@@ -2,19 +2,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using IsolateIsland.Runtime.Ai;
 
 namespace IsolateIsland.Runtime.Interact
 {
     public class EnemyEntity : HitInteractEntity
     {
-        private AITest _aI = null;
-        public AITest AI => _aI = _aI ?? GetComponent<AITest>();
+        private EnemyAI _aI = null;
+        public EnemyAI AI => _aI = _aI ?? GetComponent<EnemyAI>();
+
+        private AITest _testAI = null;
+        public AITest testAI => _testAI = _testAI ?? GetComponent<AITest>();
         protected override void OnInteractHitEvent(Entity entity)
         {
             base.OnInteractHitEvent(entity);
 
-            AI.enemyStatus.hp -= Managers.Managers.Instance.statManager.UserStat.ATK;
-            Debug.Log("========= =========Enemy HP : " + AI.enemyStatus.hp);
+            if (testAI != null)
+            {
+                testAI.enemyStatus.hp -= Managers.Managers.Instance.statManager.UserStat.ATK;
+                Debug.Log("========= =========Enemy(Test) HP : " + testAI.enemyStatus.hp);
+            }
+
+            if (AI != null)
+            {
+                AI.hp -= Managers.Managers.Instance.statManager.UserStat.ATK;
+                Debug.Log("========= =========Enemy(Test) HP : " + AI.hp);
+            }
         }
     }
 }
